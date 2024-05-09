043d082d72d2dba44f933da9b339b33085335755
IvchenkoEV <pesok72@mail.ru>
2024-05-09 18:53:03 +0500

    Изменен main

--- src/Main.java
+++ src/Main.java
@@ -14,94 +14,94 @@ public class Main {
         System.out.println("Поехали!");
         File file = Path.of("file.csv").toFile();
 
-//        final TaskManager taskManager = Managers.getDefault(file);
-//
-//        System.out.println("Проверочный вызов пустого принта");
-//        printAllTasks(taskManager);
-//
-//        Task taskOne = new Task("Задача 1", "Купить продукты");
-//        taskOne.setId(7);
-//        Task taskTwo = new Task("Задача 2", "Вынести мусор");
-//        taskTwo.setId(7);
-//        System.out.println(taskOne);
-//        System.out.println(taskTwo);
-//
-//        taskManager.getTaskById(7);
-//
-//        System.out.println("первый вызов принта");
-//        printAllTasks(taskManager);
-//
-//        Task task1 = new Task("Задача 1", "Купить продукты");
-//        taskManager.createTask(task1);
-//        Task task2 = new Task("Задача 2", "Вынести мусор");
-//        taskManager.createTask(task2);
-//
-//        Epic renovation = new Epic("Ремонт","Сделать ремонт");
-//        taskManager.createEpic(renovation);
-//        int renovationId = renovation.getId();
-//        Subtask wall = new Subtask("Стены", "Шпаклюем и штукатурим", renovationId);
-//        taskManager.createSubtask(wall);
-//        Subtask furniture = new Subtask("Мебель", "Купить и собрать", renovationId);
-//        taskManager.createSubtask(furniture);
-//
-//        Epic vacation = new Epic("Отпуск","Запланировать путешествие");
-//        taskManager.createEpic(vacation);
-//        int vacationId = vacation.getId();
-//        Subtask tickets = new Subtask("Билеты", "Найти выгодные даты", vacationId);
-//        taskManager.createSubtask(tickets);
-//
-//        taskManager.getTaskById(1);
-//        taskManager.getTaskById(2);
-//
-//        System.out.println("второй вызов принта");
-//        printAllTasks(taskManager);
-//
-//        taskManager.getTaskById(1);
-//
-//        System.out.println("вызываем несуществующую задачу:");
-//        taskManager.getTaskById(9);
-//
-//        System.out.println("третий вызов принта");
-//        printAllTasks(taskManager);
-//
-//        Task newTask1 = new Task("Задача 1", "Купить много продуктов");
-//        newTask1.setStatus(Status.DONE);
-//        newTask1.setId(1);
-//        taskManager.updateTask(newTask1);
-//        Task newTask2 = new Task("Задача 2", "Вынести мусор");
-//        newTask2.setStatus(Status.IN_PROGRESS);
-//        newTask2.setId(2);
-//        taskManager.updateTask(newTask2);
-//
-//        Subtask newWall = new Subtask("Стены", "Красим", renovationId);
-//        newWall.setStatus(Status.IN_PROGRESS);
-//        newWall.setId(4);
-//        taskManager.updateSubtask(newWall);
-//        Subtask newFurniture = new Subtask("Мебель", "Заказать", renovationId);
-//        newFurniture.setStatus(Status.DONE);
-//        newFurniture.setId(5);
-//        taskManager.updateSubtask(newFurniture);
-//
-//        Subtask newTickets = new Subtask("Билеты", "Найти выгодные даты", vacationId);
-//        newTickets.setStatus(Status.DONE);
-//        newTickets.setId(7);
-//        taskManager.updateSubtask(newTickets);
-//
-//        taskManager.getEpicById(3);
-//
-//        System.out.println("четверый вызов принта");
-//        printAllTasks(taskManager);
-//
-//        taskManager.deleteTaskById(2);
-//        taskManager.deleteEpicById(6);
-//
-//        taskManager.getSubtaskById(5);
-//
-//        System.out.println("пятый вызов принта");
-//        printAllTasks(taskManager);
+        final TaskManager taskManager = Managers.getDefault(file);
+
+        System.out.println("Проверочный вызов пустого принта");
+        printAllTasks(taskManager);
+
+        Task taskOne = new Task("Задача 1", "Купить продукты");
+        taskOne.setId(7);
+        Task taskTwo = new Task("Задача 2", "Вынести мусор");
+        taskTwo.setId(7);
+        System.out.println(taskOne);
+        System.out.println(taskTwo);
+
+        taskManager.getTaskById(7);
+
+        System.out.println("первый вызов принта");
+        printAllTasks(taskManager);
+
+        Task task1 = new Task("Задача 1", "Купить продукты");
+        taskManager.createTask(task1);
+        Task task2 = new Task("Задача 2", "Вынести мусор");
+        taskManager.createTask(task2);
+
+        Epic renovation = new Epic("Ремонт", "Сделать ремонт");
+        taskManager.createEpic(renovation);
+        int renovationId = renovation.getId();
+        Subtask wall = new Subtask("Стены", "Шпаклюем и штукатурим", renovationId);
+        taskManager.createSubtask(wall);
+        Subtask furniture = new Subtask("Мебель", "Купить и собрать", renovationId);
+        taskManager.createSubtask(furniture);
+
+        Epic vacation = new Epic("Отпуск", "Запланировать путешествие");
+        taskManager.createEpic(vacation);
+        int vacationId = vacation.getId();
+        Subtask tickets = new Subtask("Билеты", "Найти выгодные даты", vacationId);
+        taskManager.createSubtask(tickets);
+
+        taskManager.getTaskById(1);
+        taskManager.getTaskById(2);
+
+        System.out.println("второй вызов принта");
+        printAllTasks(taskManager);
+
+        taskManager.getTaskById(1);
+
+        System.out.println("вызываем несуществующую задачу:");
+        taskManager.getTaskById(9);
+
+        System.out.println("третий вызов принта");
+        printAllTasks(taskManager);
+
+        Task newTask1 = new Task("Задача 1", "Купить много продуктов");
+        newTask1.setStatus(Status.DONE);
+        newTask1.setId(1);
+        taskManager.updateTask(newTask1);
+        Task newTask2 = new Task("Задача 2", "Вынести мусор");
+        newTask2.setStatus(Status.IN_PROGRESS);
+        newTask2.setId(2);
+        taskManager.updateTask(newTask2);
+
+        Subtask newWall = new Subtask("Стены", "Красим", renovationId);
+        newWall.setStatus(Status.IN_PROGRESS);
+        newWall.setId(4);
+        taskManager.updateSubtask(newWall);
+        Subtask newFurniture = new Subtask("Мебель", "Заказать", renovationId);
+        newFurniture.setStatus(Status.DONE);
+        newFurniture.setId(5);
+        taskManager.updateSubtask(newFurniture);
+
+        Subtask newTickets = new Subtask("Билеты", "Найти выгодные даты", vacationId);
+        newTickets.setStatus(Status.DONE);
+        newTickets.setId(7);
+        taskManager.updateSubtask(newTickets);
+
+        taskManager.getEpicById(3);
+
+        System.out.println("четверый вызов принта");
+        printAllTasks(taskManager);
+
+        taskManager.deleteTaskById(2);
+        taskManager.deleteEpicById(6);
+
+        taskManager.getSubtaskById(5);
+
+        System.out.println("пятый вызов принта");
+        printAllTasks(taskManager);
 
 
-        final TaskManager taskManager = Managers.load(file);
+        /*final TaskManager taskManager = Managers.load(file);
 
         System.out.println("вызов принта восстановления из фала");
         printAllTasks(taskManager);
@@ -124,10 +124,10 @@ public class Main {
         taskManager.getTaskById(1);
 
         System.out.println("восьмой вызов принта");
-        printAllTasks(taskManager);
+        printAllTasks(taskManager);*/
     }
 
-    private static void printAllTasks(TaskManager manager) {
+    private static void printAllTasks (TaskManager manager){
         System.out.println("Задачи:");
         for (Task task : manager.getAllTasksList()) {
             System.out.println(task);
