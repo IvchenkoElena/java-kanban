319f11e1911ad4d59f66b5f7028f878f3c4c1299
IvchenkoEV <pesok72@mail.ru>
2024-05-31 21:34:51 +0500

    переименован пакет

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
-import HTTP.DurationAdapter;
-import HTTP.LocalDateTimeAdapter;
+import http.DurationAdapter;
+import http.LocalDateTimeAdapter;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
